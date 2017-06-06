cd017f75098f4c0c88b2118a2b967e44ceb495cb
tuxutku <proje.pdf@outlook.com>
2017-06-06 19:54:15 +0300

    Update finddep.sh

--- finddep.sh
+++ finddep.sh
@@ -9,13 +9,12 @@ echo debian packages
 echo "if you don't want to continue do ctrl+c"
 echo "are you want to write stdout to a file? (yes/no)"
 read v
-if [[ $v = @(y|Y|Yes|yes|YES) ]]; then echo enter the file name; read c; a=" > "; b=u; else b=d; fi
-if [[ $v = @(n|N|No|NO|no) ]]; then b=u; fi
+if [ $v = y -o $v = Y -o $v = Yes -o $v = yes -o $v = YES ]; then echo enter the file name; read c; a=" > "; b=u; else b=d; fi
+if [ $v = n -o $v = N -o $v = No -o $v = no -o $v = NO ]; then b=u; fi
 if [ $b = "d" ]; then echo wrong entry, ending; exit; fi
 echo "are you want package name placed beginning to its depencies list? (yes/no)"
 read v
-if [[ $v = @(y|Y|Yes|yes|YES) ]]; then j="echo {}; "; b=u; else b=d; fi
-if [[ $v = @(n|N|No|NO|no) ]]; then b=u; x=" | xargs"; fi
+if [ $v = y -o $v = Y -o $v = Yes -o $v = yes -o $v = YES ]; then j="echo {}; "; b=u; else b=d; fi
+if [ $v = n -o $v = N -o $v = No -o $v = no -o $v = NO ]; then b=u; x=" | xargs"; fi
 if [ $b = "d" ]; then echo wrong entry, ending; exit; fi
-#find *.deb -exec sh -c 'ar p {} control.tar.gz | tar zx -O | grep Depends | tr -d "Depends:,"' \; | xargs > depency.txt
 eval "find *.deb -exec sh -c '$j\ar p {} control.tar.gz | tar zx -O | grep Depends | tr -d "Depends:,"' \;$x$a$c"
